9303019ff452e584779e4326585ef1643a9d67b1
Shivam Verma <shivamverma21201@gmail.com>
2026-04-11 14:22:24 +0530

    fixed ui for mobile devices

--- app/layout.jsx
+++ app/layout.jsx
@@ -13,6 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com"  />
         <link href="https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&display=swap" rel="stylesheet" />
